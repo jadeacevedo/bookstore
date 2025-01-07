0a007ec28cbbf7a3e0c11426457de46646239dec
jade24321085 <jade24321085@gmail.com>
2025-01-07 19:12:37 +0000

    first commit

---
+++ app.ts
@@ -0,0 +1,34 @@
+import bookData from './bookstore.json'
+
+const Book = (book: typeof bookData[number]) => {
+    return `
+        <li> ${book.title}</li>
+    `
+}
+const Books= (books: typeof bookData) => {
+    return `
+    <ul>
+    ${books.map (book => Book (book)).join ('')}
+    </ul>
+
+    `
+}
+
+const render = (books: typeof bookData, query: string = '')=> {
+    document.getElementById('results')!.innerHTML= Books(books.filter(book => {
+        return book.title.toLowerCase().includes(query.toLowerCase())
+    }));
+}
+
+document.getElementById('search-input')?.addEventListener('keyup', (event) => {
+    const query = (event.target as HTMLInputElement).value
+    
+    render(bookData, query)
+})
+document.getElementById('search-form')?.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
+render(bookData)
+    
+
+
